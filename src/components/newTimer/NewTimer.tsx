d0578d71862f8c4897fa57df5d5a7e8492285103
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-04-10 16:22:23 +0300

    The duration field has been changed from type 'time' to type 'text', as well as the format of the entered values from --:-- to hh/mm/ss. Validations have been added to both input fields along with user messages. The code has been refactored to accommodate the new data input format.

--- src/components/newTimer/NewTimer.tsx
+++ src/components/newTimer/NewTimer.tsx
@@ -4,26 +4,45 @@ import Form from "../common/form/Form";
 import Input from "../common/input/Input";
 import Button from "../common/button/Button";
 
-export type ExtractedData = {
-    id: number;
-    name: string;
+type Placeholder = {
+    name: string,
     duration: string;
+}
+
+export type ExtractedData = Placeholder & {
+    id: number;
 }
 
 export default function NewTimer() {
 
     const { addTimer } = useTimerContext();
-    const [placeholder, setPlaceholder] = useState<string>('Yoga');
-
+    const initPlaceholder ={ name: 'Yoga', duration: 'hh/mm/ss' }
+    const [placeholder, setPlaceholder] = useState<Placeholder>(initPlaceholder);
+    
     function handleSubmit(data: unknown) {
         const extractedData = data as ExtractedData;
         const id = Math.floor(Math.random() * 1000);
-        const canAdd = extractedData.name !== "" && extractedData.duration !== "";
-        
+        const isValidDurationFormat = /^([01]\d|2[0-3])\/([0-5]\d)\/([0-5]\d)$/.test(extractedData.duration);
+        const canAdd = extractedData.name !== '' && extractedData.duration !== ''&&  isValidDurationFormat;
+
         if (canAdd) {
             addTimer({ id: id, name: extractedData.name, duration: extractedData.duration });
         } else {
-            setPlaceholder('All fields are required!');
+
+            let newName = placeholder.name;
+            let newDuration = placeholder.duration;
+
+            if (extractedData.name === '') {
+                newName = 'All fields are required!';
+            }
+            if ( !isValidDurationFormat){
+                newDuration = 'Invalid format.'
+            }
+            if (extractedData.duration === '') {
+                newDuration = 'All fields are required!';
+            }
+
+            setPlaceholder({ name: newName, duration: newDuration });
         }
     }
 
@@ -33,15 +52,15 @@ export default function NewTimer() {
                 type="text"
                 id="name"
                 label="Enter the Name"
-                placeholder={placeholder}
-                onFocus={() => setPlaceholder('')}
-                onBlur={() => setPlaceholder('Yoga')}
+                placeholder={placeholder.name}
+                onBlur={() => setPlaceholder(initPlaceholder)}
             />
             <Input
-                type="time"
-                min="00:00" max="12:00" step="300"
+                type="text"
                 id="duration"
                 label="Enter the Duration"
+                placeholder={placeholder.duration}
+                onBlur={() => setPlaceholder(initPlaceholder)}
             />
             <Button>Add New Timer</Button>
         </Form>
--- src/components/singleTimer/SingleTimer.tsx
+++ src/components/singleTimer/SingleTimer.tsx
@@ -3,11 +3,11 @@ import { type ExtractedData as TimerProps } from "../newTimer/NewTimer";
 import useTimerContext from "../../store/useTimerContext";
 
 export default function SingleTimer({ id, name, duration }: TimerProps) {
-    const hours = Number(duration.split(':')[0]);
-    const minutes = Number(duration.split(':')[1]);
-    const durationInMinutes = (hours * 60 + minutes) * 1000;
+   
+    const [inputHours, inputMinutes, inputSeconds] = duration.split('/').map(Number);
+    const durationInSeconds = inputHours * 360 + inputMinutes * 60 + inputSeconds;
 
-    const [time, setTime] = useState(durationInMinutes);
+    const [time, setTime] = useState(durationInSeconds);
     const interval = useRef<number | null>(null);
 
     const { isRunning, deleteTimer } = useTimerContext();
@@ -21,9 +21,9 @@ export default function SingleTimer({ id, name, duration }: TimerProps) {
                     if (prevTime <= 0 || prevTime === null) {
                         return 0;
                     }
-                    return prevTime - 100;
+                    return prevTime - 1;
                 });
-            }, 100);
+            }, 1000); 
 
             interval.current = timer;
 
@@ -34,17 +34,12 @@ export default function SingleTimer({ id, name, duration }: TimerProps) {
         return () => clearInterval(timer);
     }, [isRunning]);
 
-    const remainingHours = Math.floor(time / 1000 / 60).toString().padStart(2, '0');
-
-    let remainingMin: string;
-    if (time <= 0) {
-        remainingMin = Math.floor((time / 1000) % 60).toString().padStart(2, '0');
-    } else {
-        remainingMin = Math.floor((time / 1000) % 60 + 1).toString().padStart(2, '0');
-    }
+    const remainingHours = Math.floor(time / 3600).toString().padStart(2, '0');
+    const remainingMinutes = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
+    const remainingSeconds = (time % 60).toString().padStart(2, '0');
 
     function reset() {
-        setTime(durationInMinutes);
+        setTime(durationInSeconds); // Връщаме времето на началната стойност в секунди
     }
 
     function deleteTimerById() {
@@ -55,8 +50,8 @@ export default function SingleTimer({ id, name, duration }: TimerProps) {
         <article>
             <button onClick={deleteTimerById}>X</button>
             <h2>{time ? `${name}` : `${name} finished`}</h2>
-            <p><progress max={durationInMinutes} value={time} /></p>
-            <p><span>{`${remainingHours}:${remainingMin}`}</span><span onClick={reset}>reset</span></p>
+            <p><progress max={durationInSeconds} value={time} /></p>
+            <p><span>{`${remainingHours}:${remainingMinutes}:${remainingSeconds}`}</span><span onClick={reset}>reset</span></p>
         </article>
     );
 }
