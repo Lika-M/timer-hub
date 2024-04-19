e6e8c52244bf5d82a881d21f90b8a4bd0815e415
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-04-19 12:31:19 +0300

    Updated the header and added animation.

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -4,8 +4,11 @@ export default function Header() {
 
     return (
         <header className="header">
-            <h1>React Timer Hub</h1>
-            
+            <h2>
+                <span>Time Management </span>
+                <span>Techniques for</span>
+                <span> Busy People</span>
+            </h2>
         </header>
     );
 }
